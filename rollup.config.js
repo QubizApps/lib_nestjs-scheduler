7b89d0341c84fe201ee9e0dcdc9db2a94c6294df
tiberiu89 <tiberiu.popa@qubiz.com>
2022-07-28 19:06:25 +0300

    initial commit

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import typescript from '@rollup/plugin-typescript';
+import path from 'path';
+import del from 'rollup-plugin-delete';
+import dts from 'rollup-plugin-dts';
+
+import pkg from './package.json';
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default [
+  {
+    input: 'src/index.ts',
+    plugins: [
+      del({ targets: ['./dist'], runOnce: !production, }),
+      typescript({
+        tsconfig: path.join(__dirname, 'tsconfig.json'),
+        declaration: true,
+        declarationDir: 'dts'
+      }),
+    ],
+    output: [
+      { file: pkg.main, format: 'cjs', sourcemap: true },
+    ],
+    external: [/@qubizapps\/.*/, /@nestjs\/.*/, /node:.*/, 'lodash'],
+  },
+  // bundle all type definitions into one file
+  {
+    input: 'dist/dts/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'es' }],
+    plugins: [
+      dts(),
+      del({
+        targets: ['./dist/dts'],
+        hook: 'buildEnd',
+      }),
+    ],
+  },
+];
